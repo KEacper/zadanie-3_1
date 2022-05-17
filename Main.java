797f62a1e94bb366e60e82e1aaefe97c8538ce3f
KEacper <101186813+KEacper@users.noreply.github.com>
2022-05-17 11:18:22 +0000

    3_1a 3_1b

--- Main.java
+++ Main.java
@@ -1,16 +1,54 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
+      Scanner skan = new Scanner(System.in);
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
-      var students = s.getStudents();
+      int x=4;
+      String imie;
+      String nazwisko;
+      int wiek;
+      String uro;
+      System.out.println("Lista studentów");
+      while(x!=0)
+        {
+          System.out.println("Wybierz funkcję: \n"+
+                        "1. wprowadzanie studenta \n"+
+                        "2. wypisanie listy studentów \n"+
+                        "3. zamknięcie programu");
+          x=skan.nextInt();
+          switch(x)
+            {
+                case 1:
+                {
+                  Scanner skan1 = new Scanner(System.in);
+                  System.out.println("Podaj imie: ");
+                  imie=skan1.nextLine();
+                  System.out.println("Podaj nazwisko: ");
+                  nazwisko=skan1.nextLine();
+                  System.out.println("Podaj wiek: ");
+                  wiek=skan.nextInt();
+                  System.out.println("Podaj datę urodzenia: ");
+                  uro=skan1.nextLine();
+                  s.addStudent(new Student(imie, nazwisko, wiek, uro));
+                }break;
+              case 2:
+                { System.out.println("Wszyscy studenci w bazie: \n");
+                   var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
       }
+                }break;
+              case 3:
+                {
+                  x=0;
+                }break;
+            }
+            
+        }
+      
     } catch (IOException e) {
 
     }
--- Student.java
+++ Student.java
@@ -1,24 +1,31 @@
 public class Student {
 
   private String Name;
+  private String Nazwisko;
   private int Age;
+  private String Uro;
 
-  public Student(String name, int age) {
+  public Student(String name, String nazwisko, int age, String uro) {
     Name = name;
+    Nazwisko = nazwisko;
     Age = age;
+    Uro = uro;
+    
   }
 
   public String GetName() {return Name;}
+  public String GetNazwisko() {return Nazwisko;}
   public int GetAge() {return Age;}
+   public String GetBorn() {return Uro;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return Name + " " + Nazwisko + " " + Integer.toString(Age)+" "+Uro;
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if(data.length != 4) 
+      return new Student("Parse Error","Parse Error", -1,"Parse Error");
+    return new Student(data[0], data[1], Integer.parseInt(data[2]),data[3]);
   }
 }
